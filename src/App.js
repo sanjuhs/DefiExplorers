63c3374227efe4de8b2823c78cf5c7158dba7d14
sanjayprasad s <sanjayprasads@sanjayprasads-MacBook-Air.local>
2022-05-21 14:25:42 +0530

    the basic submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "/build/build5.loader.js",
+  dataUrl: "/build/build5.data.unityweb",
+  frameworkUrl: "/build/build5.framework.js.unityweb",
+  codeUrl: "/build/build5.wasm.unityweb",
+});
+
+// var buildUrl = "Build";
+//       var loaderUrl = buildUrl + "/build1.loader.js";
+//       var config = {
+//         dataUrl: buildUrl + "/build1.data.gz",
+//         frameworkUrl: buildUrl + "/build1.framework.js.gz",
+//         codeUrl: buildUrl + "/build1.wasm.gz",
+//         streamingAssetsUrl: "StreamingAssets",
+//         companyName: "DefaultCompany",
+//         productName: "2dRPG_1_test",
+//         productVersion: "1.0",
+//         showBanner: unityShowBanner,
+//       };
+// this is a test only yeah its tru terminal ?
 
 function App() {
   return (
@@ -15,8 +38,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with this app
         </a>
+        <Unity unityContext={unityContext} id="unityg" />
       </header>
     </div>
   );
